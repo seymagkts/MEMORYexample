15f924f94f9c8b47b6b761001f391bcbfce86ba6
Seyma <63148309+lacilobaid@users.noreply.github.com>
2021-08-13 17:22:19 +0300

    Create card.java

---
+++ card.java
@@ -0,0 +1,24 @@
+public class kart {
+    private char deger;
+    private boolean tahmin=false;
+
+    public kart (char deger){
+        this.setDeger(deger);
+    }
+
+    public char getDeger() {
+        return deger;
+    }
+
+    public void setDeger(char deger) {
+        this.deger = deger;
+    }
+
+    public boolean isTahmin() {
+        return tahmin;
+    }
+
+    public void setTahmin(boolean tahmin) {
+        this.tahmin = tahmin;
+    }
+}
